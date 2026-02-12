265013ecb5b1df6225dc4f21890df9b4d26fca97
joakimchi <joakim.nguyen99@gmail.com>
2026-02-11 22:51:15 -0600

    visualization: export QC functions and tolerate optional DGL import

--- histocartography_ext/visualization/__init__.py
+++ histocartography_ext/visualization/__init__.py
@@ -1,11 +1,27 @@
-from .visualization import (
-    OverlayGraphVisualization,
-    InstanceImageVisualization,
-    HACTVisualization,
+try:
+    from .visualization import (
+        OverlayGraphVisualization,
+        InstanceImageVisualization,
+        HACTVisualization,
+    )
+except Exception:
+    # Allow import to succeed if DGL is missing/broken, 
+    # but these classes won't be available.
+    pass
+
+from .qc import (
+    generate_qc_thumbnail,
+    compute_qc_metrics,
+    save_qc_metrics,
+    plot_qc_distributions
 )
 
 __all__ = [
     "OverlayGraphVisualization",
     "InstanceImageVisualization",
     "HACTVisualization",
+    "generate_qc_thumbnail",
+    "compute_qc_metrics",
+    "save_qc_metrics",
+    "plot_qc_distributions",
 ]
